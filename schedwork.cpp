59295c6ad7b961abb02aadf06b0a2ef007ab74f9
Omer Ugural <ougural@usc.edu>
2022-12-04 06:56:56 +0000

    hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -1,5 +1,3 @@
-
-
 #include <set>
 #include <iostream>
 #include <fstream>
@@ -20,6 +18,19 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
+bool helper(
+    const AvailabilityMatrix& avail,
+    const size_t maxShifts,
+    DailySchedule& sched, 
+    int shiftCurr,
+    int dayCurr);
+
+bool check(const AvailabilityMatrix& avail,
+    const size_t maxShifts,
+    const DailySchedule sched,
+    int shiftCurr,
+    int dayCurr);
+
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -27,17 +38,88 @@ bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
     const size_t maxShifts,
-    DailySchedule& sched
-)
+    DailySchedule& sched)
 {
     if(avail.size() == 0U){
-        return false;
+      return false;
     }
     sched.clear();
-    // Add your code below
+  
+    // calculate n (number of days)
+    int numDays = 0;
+    for(int i = 0; i < avail.size(); i++){
+        numDays++;
+    }
 
+    // initialize an n x d schedule with dummy vars
+    Worker_T dummy = -1;
+    // loop through number of days
+    for(unsigned int i = 0; i < numDays; i++){
+        vector<Worker_T> temp;
+        // for each day, add a d-lenghted vector
+        for(unsigned int j = 0; j < dailyNeed; j++){
+            temp.push_back(dummy);
+        }
+        sched.push_back(temp);
+    }
 
+    return helper(avail, maxShifts, sched, 0, 0);
+}
 
+bool helper(
+    const AvailabilityMatrix& avail,
+    const size_t maxShifts,
+    DailySchedule& sched, 
+    int shiftCurr,
+    int dayCurr)
+{
+    // increase current day, reset current shift
+    if(shiftCurr == sched[0].size()){
+        shiftCurr = 0;
+        dayCurr++;
+    }
+    // return true if recursed everyday
+    if(dayCurr == sched.size()){
+        return 1;
+    }
+    for(Worker_T id = 0; id < avail[0].size(); id++){
+        sched[dayCurr][shiftCurr] = id;
+	    //  check
+        if(check(avail, maxShifts, sched, shiftCurr, dayCurr)){
+            shiftCurr++;
+            return helper(avail, maxShifts, sched, shiftCurr, dayCurr);
+        }
+    }
+    return 0;
+}
 
+bool check(
+    const AvailabilityMatrix& avail,
+    const size_t maxShifts,
+    const DailySchedule sched,
+    int shiftCurr,
+    int dayCurr)
+{
+    Worker_T id = sched[dayCurr][shiftCurr];
+    // count number of shifts worker has
+    int numShifts = 0;
+    for(int i = 0; i < sched.size(); i++){
+        for(int j = 0; j < sched[0].size(); j++){
+            if(sched[i][j] == id){
+                numShifts++;
+            }
+        }
+    }
+    // check if number of shifts exceeds max 
+    bool exceedMaxShift = false;
+    if(numShifts > maxShifts){
+        exceedMaxShift = true;
+    }
+    // check if worker is available for the day
+    bool available = false;
+    if(avail[dayCurr][id]){
+        available = true;
+    }
+    return (available && !exceedMaxShift);
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -6,20 +6,145 @@
 #include <set>
 #include "wordle.h"
 #include "dict-eng.h"
+#include "vector"
 using namespace std;
 
 
 // Add prototypes of helper functions here
+void findDashCombs(
+    int numDashes,
+    std::string prefix,
+    std::set<std::string> &dashCombs,
+    std::string alphabet);
+
+std::set<std::string> combined(
+  std::set<std::string> &dashCombs, 
+	const std::string &in);
+
+std::set<std::string> filterNonFloats(
+  std::set<std::string> &fullCombs, 
+	const std::string &floating);
+
+std::set<std::string> filterNonWords(
+  std::set<std::string> &firstFilter, 
+	const std::set<std::string> &dict);
 
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
-    const std::string& in,
-    const std::string& floating,
-    const std::set<std::string>& dict)
+    const std::string &in,
+    const std::string &floating,
+    const std::set<std::string> &dict)
 {
-    // Add your code here
-
+    // find num dashes and fixed indeces
+    int numDashes = 0;
+    for(int i = 0; i < in.size(); i++){
+      if(in[i] == '-'){
+        numDashes++;
+      }
+    }
+    // find all combs for dash spaces
+    std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    std::set<std::string> dashCombs;
+    findDashCombs(numDashes, "", dashCombs, alphabet);
+    // combine dash combs and fixeds into a new set
+		std::set<std::string> fullCombs = combined(dashCombs, in);
+    // filter non floats
+		std::set<std::string> firstFilter = filterNonFloats(fullCombs, floating);
+		// filter non words
+		std::set<std::string> secondFilter = filterNonWords(firstFilter, dict);
+		return secondFilter;
 }
 
 // Define any helper functions here
+
+std::set<std::string> filterNonWords(
+  std::set<std::string> &firstFilter, 
+	const std::set<std::string> &dict)
+{
+	std::set<std::string> returnThis;
+	std::set<std::string>::iterator it;
+	for(it = firstFilter.begin(); it != firstFilter.end(); ++it){
+		if(dict.find(*it) != dict.end()){
+			// cout << *it << endl;
+			returnThis.insert(*it);
+		}
+	}
+	return returnThis;
+}
+
+
+std::set<std::string> filterNonFloats(
+  std::set<std::string> &fullCombs, 
+	const std::string &floating)
+	{
+		std::set<std::string> returnThis;
+		std::set<std::string>::iterator it;
+		for(it = fullCombs.begin(); it != fullCombs.end(); ++it){
+			int ctr = 0;
+			std::string temp = floating;
+			for(int i = 0; i < (*it).size(); i++){
+				for(int j = 0; j < temp.size(); j++){
+					if((*it)[i] == temp[j]){
+						temp[j] = '0';
+						ctr++;
+					}
+				}
+			}
+			if(ctr == temp.size()){
+				// cout << *it << endl;
+				returnThis.insert(*it);
+			}
+		}
+		return returnThis;
+	}
+
+void findDashCombs(
+    int numDashes,
+    std::string prefix,
+    std::set<std::string> &dashCombs,
+    std::string alphabet
+  )
+{ 
+  if(prefix.size() == numDashes){
+    dashCombs.insert(prefix);
+  }
+  else if(prefix.size() < numDashes){
+    for(auto l : alphabet){
+      findDashCombs(numDashes, prefix + l, dashCombs, alphabet); 
+    }
+  }
+}
+
+std::set<std::string> combined(
+  std::set<std::string> &dashCombs, 
+  const std::string &in)
+{
+  std::set<std::string> returnThis;
+
+  std::set<std::string>::iterator it;
+  int ctr = 0;
+  for(it = dashCombs.begin(); it != dashCombs.end(); ++it){
+    ctr = 0;
+    std::string temp = "";
+    for(int i = 0; i < in.size(); i++){
+			if(in[i] != '-'){
+        temp += in[i];
+      }
+      else{
+        temp += (*it)[ctr];
+        ctr++;
+      }
+    }
+		// cout << temp << endl;
+    returnThis.insert(temp);
+  }
+  return returnThis;  
+}
+
+
+
+
+
+
+
